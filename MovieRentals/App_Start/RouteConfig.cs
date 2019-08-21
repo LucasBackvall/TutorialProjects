db0441fa65a14558da17452de6d36d47df6594bc
Lucas Bäckvall <lucas.backvall@gmail.com>
2019-08-21 17:37:40 +0200

    Started messing around with routing. Check in RouteConfig.cs and MoviesController.cs

--- MovieRentals/App_Start/RouteConfig.cs
+++ MovieRentals/App_Start/RouteConfig.cs
@@ -7,17 +7,27 @@ using System.Web.Routing;
 
 namespace MovieRentals
 {
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-        }
-    }
+			// Special route for MoviesByReleaseDate.
+			// Constrained so a year must be 4 digits and month must be 2 digits.
+			//  2200 /99 is still valid.
+			routes.MapRoute(
+				name: "MoviesByReleaseDate",
+				url: "movies/released/{year}/{month}",
+				defaults: new { controller = "Movies", action = "ByReleaseDate" },
+				new { year = @"\d{4}", month = @"\d{2}" }
+			);
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+			);
+		}
+	}
 }
+ 
---
+++ MovieRentals/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRentals.Models;
+
+namespace MovieRentals.Controllers
+{
+	public class MoviesController : Controller
+	{
+		// Example of routing with default routing
+		public ActionResult Random()
+		{
+			var movie = new Movie() { Name = "Lion King live action remake" };
+			return View(movie);
+		}
+
+		public ActionResult Edit(int id)
+		{
+			return Content("id=" + id);
+		}
+		
+		public ActionResult Index(int? pageIndex, string sortBy)
+		{
+			if (!pageIndex.HasValue)
+				pageIndex = 1;
+			if (String.IsNullOrWhiteSpace(sortBy))
+				sortBy = "Name";
+			return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+		}
+
+		// Special case routing
+		public ActionResult ByReleaseDate(int year, int month)
+		{
+			return Content(String.Format("Movies released {0}/{1}", year, month));
+		}
+
+	}
+}
